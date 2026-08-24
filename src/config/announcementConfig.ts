90f2147360a984ab26b53512ac614dc5560cf8f6
山吹 <2919099621@qq.com>
2026-08-24 21:12:56 +0800

    vault backup: 2026-08-24 21:12:55

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到Fem-boy_Sc的博客！",
+	content: "欢迎来到Fem-boy_Sc的博客!",
 
 	// 是否允许用户关闭公告
 	closable: true,
