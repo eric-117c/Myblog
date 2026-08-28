300e1b35a52767500aa45f4a1b49e684fc92bc35
山吹 <2919099621@qq.com>
2026-08-28 15:19:12 +0800

    bckground

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -39,29 +39,29 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/d1.jpg",
+			"assets/images/DesktopWallpaper/d2.jpg",
+			"assets/images/DesktopWallpaper/d3.jpg",
+			"assets/images/DesktopWallpaper/d4.jpg",
+			"assets/images/DesktopWallpaper/d5.jpg",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/m1.jpg",
+			"assets/images/MobileWallpaper/m2.jpg",
+			"assets/images/MobileWallpaper/m3.jpg",
+			"assets/images/MobileWallpaper/m4.jpg",
+			"assets/images/MobileWallpaper/m5.jpg",
+			"assets/images/MobileWallpaper/m6.jpg",
+			"assets/images/MobileWallpaper/m7.jpg",
+			"assets/images/MobileWallpaper/m8.jpg",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
 		// 支持远程视频URL，本地视频请放在 public/assets/videos/ 目录下
 		// playerUrl: "/assets/videos/firefly.mp4",
 		playerUrl: [
-			"https://www.image2url.com/r2/default/videos/1781765166391-f2ba6648-1597-40e0-9f0a-6768ae39e574.mp4",
 		],
 	},
 	// 横幅壁纸和全屏壁纸共享配置
